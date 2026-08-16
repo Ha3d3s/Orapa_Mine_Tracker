f6d4a4c27c58baa111440ce374c5d0f182208a5a
Ha3d3s <collard.nicolas92@gmail.com>
2026-08-16 16:23:32 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: "./" makes the build work when hosted in a GitHub Pages
-// project subfolder (https://username.github.io/repo-name/)
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-});
